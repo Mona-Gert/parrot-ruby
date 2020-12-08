4e7cfaac6fea327306897d4b2b623f0be0d7855a
monamohamed <mona.m.mohamed@gmail.com>
2020-12-07 23:13:13 -0500

    commit tree 42b70feb87599ca4e184d2aef2b18b607454ac23

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+#Define a method that accepts arguments.
+def parrot (bird_sound = "Squawk!")
+puts bird_sound 
+return bird_sound
+end
